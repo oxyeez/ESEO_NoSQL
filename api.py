ab55e566e63bcd2645a3d1e8709210e7e0ae7791
Simon Duperray <simon.duperray@reseau.eseo.fr>
2022-12-06 17:28:09 +0100

    :sparkles: add dotenv file and loading config

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, json, Response
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 
 app = Flask(__name__)
-
-
-@app.route("/update")
