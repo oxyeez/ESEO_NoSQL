6b95c978fd3bb6ecbcf0f791391fc8f6354de5d5
Simon Duperray <simon.duperray@reseau.eseo.fr>
2022-12-07 09:35:27 +0100

    :sparkles: GET requests, JSON config file

--- api.py
+++ api.py
@@ -1,11 +1,30 @@
 from flask import Flask, request, json, Response
 from pymongo import MongoClient
-from dotenv import load_dotenv
+from fastapi import HTTPException, status
 import os
+import json
 
-load_dotenv()
+with open("./config.json", "r") as outfile:
+    config = json.load(outfile)
 
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+DATABASE_NAME = config["DATABASE_NAME"]
+COLLECTION_NAME = config["COLLECTION_NAME"]
+MONGODB_PORT = config["MONGODB_PORT"]
 
-app = Flask(__name__)
+
+class MongoAPI:
+    def __init__(self):
+        self.client = MongoClient('mongodb://localhost:{}/'.format(MONGODB_PORT))
+        self.db = getattr(self.client, DATABASE_NAME)
+        self.collection = getattr(self.db, COLLECTION_NAME)
+
+    def get_artists(self):
+        return self.collection.find(limit=1000)
+
+    def get_artist(self, id_artist):
+        if (artist := self.collection.find_one({ "_id": id_artist })) is not None:
+            return artist
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Artist with id {id} not found")
+
+
+api = MongoAPI()
