f0310111d20b32784cc9affd7a6ca743bc2a531c
oxyeez <marcelinliehn@gmail.com>
2023-01-10 12:35:16 +0100

    Add routes for movies

--- api/models.py
+++ api/models.py
@@ -84,7 +84,7 @@ class Tomatoes(BaseModel):
 
 
 class Movie(BaseModel):
-    #id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     #TODO maybe find a way to display the id
     awards: Optional[Awards] = None
     cast: Optional[List[str]] = None
@@ -107,3 +107,172 @@ class Movie(BaseModel):
     writers: Optional[List[str]] = None
     year: Optional[int] = None
 
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "_id": "573a1391f29313caabcd8ce8",
+                "awards": {
+                    "win": 1,
+                    "nominations": 2,
+                    "text": "1 win."
+                },
+                "cast": [
+                    "Buster Keaton",
+                    "Tom McGuire",
+                    "Ernest Torrence",
+                    "Tom Lewis"
+                ],
+                "countries": [
+                    "USA"
+                ],
+                "directors": [
+                    "Charles Reisner",
+                    "Buster Keaton"
+                ],
+                "fullplot": "Following through on a promise to his mother, William Canning Jr. goes to River Junction to meet his father who has not seen him since he was a child. The younger Canning isn't quite what the elder was expecting but the old man has bigger problems. He's being put out of business by J.J. King, who not only owns the local hotel and bank, but has recently introduced a new paddle wheel steamer that puts Cannings older boat, the Stonewall Jackson, to shame. Bill Jr. and Kitty King take a liking to each other much to the dismay of both of their fathers. When a fierce storm hits River Junction, Bill Jr. is forced to save Kitty, her father and his father.",
+                "genres": [
+                    "Action",
+                    "Comedy",
+                    "Drama"
+                ],
+                "imdb": {
+                    "rating": 8,
+                    "votes": 8617,
+                    "id": 19421
+                },
+                "languages": [
+                    "English"
+                ],
+                "lastupdated": "2015-09-07 01:03:31.287000000",
+                "num_mflix_comments": 0,
+                "plot": "The effete son of a cantankerous riverboat captain comes to join his father's crew.",
+                "poster": "https://m.media-amazon.com/images/M/MV5BOTg2MjUyMjYyOV5BMl5BanBnXkFtZTgwNjM0NDAwMjE@._V1_SY1000_SX677_AL_.jpg",
+                "rated": "NOT RATED",
+                "released": "1928-05-20T00:00:00",
+                "runtine": None,
+                "title": "Steamboat Bill, Jr.",
+                "tomatoes": {
+                    "consensus": None,
+                    "critic": {
+                        "meter": 100,
+                        "numReviews": 17,
+                        "rating": 9
+                    },
+                    "dvd": "2001-11-20T00:00:00",
+                    "fresh": 17,
+                    "lastUpdated": "2015-09-15T17:02:42",
+                    "production": "United Artists",
+                    "rotten": 0,
+                    "viewer": {
+                        "rating": 4.1,
+                        "numReviews": 5283,
+                        "meter": 92
+                    },
+                    "website": None
+                },
+                "type": "movie",
+                "writers": [
+                    "Carl Harbaugh (story)"
+                ],
+                "year": 1928
+            }
+        }
+
+
+class MovieUpdate(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    awards: Optional[Awards] = None
+    cast: Optional[List[str]] = None
+    countries: Optional[List[str]] = None
+    directors: Optional[List[str]] = None
+    fullplot: Optional[str] = None
+    genres: Optional[List[str]] = None
+    imdb: Optional[IMDB] = None
+    languages: Optional[List[str]] = None
+    lastupdated: Optional[str] = None
+    num_mflix_comments: Optional[int] = None
+    plot: Optional[str] = None
+    poster: Optional[str] = None
+    rated: Optional[str] = None
+    released: Optional[datetime] = None
+    runtine: Optional[int] = None
+    title: Optional[str] = None
+    tomatoes: Optional[Tomatoes] = None
+    type: Optional[str] = Field(...)
+    writers: Optional[List[str]] = None
+    year: Optional[int] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "awards": {
+                    "win": 1,
+                    "nominations": 2,
+                    "text": "1 win."
+                },
+                "cast": [
+                    "Buster Keaton",
+                    "Tom McGuire",
+                    "Ernest Torrence",
+                    "Tom Lewis"
+                ],
+                "countries": [
+                    "USA"
+                ],
+                "directors": [
+                    "Charles Reisner",
+                    "Buster Keaton"
+                ],
+                "fullplot": "Following through on a promise to his mother, William Canning Jr. goes to River Junction to meet his father who has not seen him since he was a child. The younger Canning isn't quite what the elder was expecting but the old man has bigger problems. He's being put out of business by J.J. King, who not only owns the local hotel and bank, but has recently introduced a new paddle wheel steamer that puts Cannings older boat, the Stonewall Jackson, to shame. Bill Jr. and Kitty King take a liking to each other much to the dismay of both of their fathers. When a fierce storm hits River Junction, Bill Jr. is forced to save Kitty, her father and his father.",
+                "genres": [
+                    "Action",
+                    "Comedy",
+                    "Drama"
+                ],
+                "imdb": {
+                    "rating": 8,
+                    "votes": 8617,
+                    "id": 19421
+                },
+                "languages": [
+                    "English"
+                ],
+                "lastupdated": "2015-09-07 01:03:31.287000000",
+                "num_mflix_comments": 0,
+                "plot": "The effete son of a cantankerous riverboat captain comes to join his father's crew.",
+                "poster": "https://m.media-amazon.com/images/M/MV5BOTg2MjUyMjYyOV5BMl5BanBnXkFtZTgwNjM0NDAwMjE@._V1_SY1000_SX677_AL_.jpg",
+                "rated": "NOT RATED",
+                "released": "1928-05-20T00:00:00",
+                "runtine": None,
+                "title": "Steamboat Bill, Jr.",
+                "tomatoes": {
+                    "consensus": None,
+                    "critic": {
+                        "meter": 100,
+                        "numReviews": 17,
+                        "rating": 9
+                    },
+                    "dvd": "2001-11-20T00:00:00",
+                    "fresh": 17,
+                    "lastUpdated": "2015-09-15T17:02:42",
+                    "production": "United Artists",
+                    "rotten": 0,
+                    "viewer": {
+                        "rating": 4.1,
+                        "numReviews": 5283,
+                        "meter": 92
+                    },
+                    "website": None
+                },
+                "type": "movie",
+                "writers": [
+                    "Carl Harbaugh (story)"
+                ],
+                "year": 1928
+            }
+        }
--- api/movies_routes.py
+++ api/movies_routes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Request, HTTPException, status
+from fastapi import APIRouter, Request, Response, HTTPException, status, Body
+from fastapi.encoders import jsonable_encoder
 from typing import List
 
-from models import Movie
+from models import Movie, MovieUpdate, ObjectId
 
 router = APIRouter()
 
@@ -11,10 +12,49 @@ def list_movies(request: Request):
     return list(request.app.database["movies"].find(limit=100))
 
 
+@router.get("/movie/{id}", response_description="Get a single movie", response_model=Movie)
+def find_artist(id: str, request: Request):
+    if (movie := request.app.database["movies"].find_one({"_id": ObjectId(id)})) is not None:
+        return movie
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie with id {id} not found")
+
+
 @router.get("/filter", response_description="Filter movies", response_model=List[Movie])
 def find_with_filter(request: Request, title: str = "", actors: str = ""):
     if actors != "":
-        return list(request.app.database["movies"].find({"title": {"$regex":title},
-                                                        "cast": {"$all": actors.split(',')}}))
+        return list(request.app.database["movies"].find({"title": {"$regex": title},
+                                                         "cast": {"$all": actors.split(',')}}))
     else:
         return list(request.app.database["movies"].find({"title": {"$regex": title}}))
+
+
+@router.delete("/{id}", response_description="Delete an movie")
+def delete_movie(id: str, request: Request, response: Response):
+    delete_result = request.app.database["movies"].delete_one({"_id": ObjectId(id)})
+    if delete_result.deleted_count == 1:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie with id {id} not found")
+
+
+@router.post("/", response_description="Create a new movie", status_code=status.HTTP_201_CREATED, response_model=Movie)
+def create_movie(request: Request, movie: MovieUpdate = Body(...)):
+    movie = jsonable_encoder(movie)
+    new_movie = request.app.database["movies"].insert_one(movie)
+    if (created_movie := request.app.database["movies"].find_one({"_id": new_movie.inserted_id})) is not None:
+        return created_movie
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie not created")
+
+
+@router.put("/{id}", response_description="Update a movie", response_model=Movie)
+def update_movie(id: str, request: Request, movie: MovieUpdate = Body(...)):
+    movie = {k: v for k, v in movie.dict().items() if v is not None}
+    if len(movie) >= 1:
+        update_result = request.app.database["movies"].update_one(
+            {"_id": ObjectId(id)}, {"$set": movie}
+        )
+        if update_result.modified_count == 0:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie with id {id} not found")
+    if (existing_movie := request.app.database["movies"].find_one({"_id": ObjectId(id)})) is not None:
+        return existing_movie
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie with id {id} not found")
